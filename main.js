dd6d1f9bdc3024da548fc5498e92dd7b242d979f
Tappu-R <tappubrain@gmail.com>
2026-05-08 20:48:34 +0530

    Leave as it is

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 const { app, BrowserWindow } = require('electron')
+
+app.whenReady().then(() => {
+    const orbWindow = new BrowserWindow({
+        width:80,
+        height:80,
+        frame:false,
+        // transparent:true,
+        alwaysOnTop:true,
+        resizable:false,
+    
+        hasShadow:false,
+        
+        webPreferences: {
+            devTools:false,
+            nodeIntegration:true,
+            contextIsolation:false
+        }
+        
+    })
+    orbWindow.loadFile('index.html')
+})
