83eaa0898944a2683f410d814755ca84cdb1a8c7
Tappu-R <tappubrain@gmail.com>
2026-05-10 09:15:17 +0530

    made the basic orb and overlay

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, screen } = require('electron')
+
+function createOrb () {
+    const orb = new BrowserWindow({
+        width: 60,
+        height: 60,
+        alwaysOnTop: true,
+        resizable: false,
+        frame: false
+    })
+    orb.loadFile('orb.html')
+}
+
+function overlayWindow () {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize
+    const overlay = new BrowserWindow({
+        width: width,
+        height: height,
+        resizable: false,
+        transparent: true,
+
+        titleBarStyle: 'hidden',
+        titleBarOverlays: true
+    })
+        
+    overlay.loadFile('overlay.html')
+    overlay.webContents.openDevTools()
+}
+
 
 app.whenReady().then(() => {
-    const orbWindow = new BrowserWindow({
-        width:80,
-        height:80,
-        frame:false,
-        // transparent:true,
-        alwaysOnTop:true,
-        resizable:false,
-    
-        hasShadow:false,
-        
-        webPreferences: {
-            devTools:false,
-            nodeIntegration:true,
-            contextIsolation:false
-        }
-        
-    })
-    orbWindow.loadFile('index.html')
+    createOrb()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit()
+    }
 })
