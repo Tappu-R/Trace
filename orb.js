52f8f718a727e7b33fc197370fd8ccff4eaba548
Tappu-R <tappubrain@gmail.com>
2026-05-12 10:25:46 +0530

    Decide draging logic steps not implement

--- orb.js
+++ orb.js
@@ -1,2 +1,13 @@
 const body = document.querySelector('body')
 
+// Mechanism
+// double mousedown par draging function invoked
+// double mousedown ke bad ke wale mouseup par posintion ko restore kar 
+// upar wala step sayad electron-window-state module se ho jaye to mouseup ka use karne ki naubat na aaye
+// single click pe overlay open kar dega
+
+let stPosisionX;
+let stPosisionY;
+body.addEventListener('mousedown', () => {
+    
+}, true)
