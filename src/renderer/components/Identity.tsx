c20bcf237857ac7ad8cec46e0754529190b0675c
Tappu-R <tappubrain@gmail.com>
2026-09-08 11:45:14 +0530

    component making

---
+++ src/renderer/components/Identity.tsx
@@ -0,0 +1,12 @@
+function Identity () {
+    return (
+        <>
+        <div className="">
+            <img src=".\assets\logo\logo.svg" alt="Logo" />
+        </div>
+            
+        </>
+    )
+}
+
+export default Identity
